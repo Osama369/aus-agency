839668857fb8b0556cd4dcd4f9e07afc0108dc52
Osama369 <umuhammadusamaqureshi@gmail.com>
2024-06-07 21:14:43 +0500

    updated

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -38,22 +38,22 @@ function Page() {
                   alt={service.title}
                 />
               </CardItem>
-              <div className="flex justify-between items-center mt-20">
-                <CardItem
+              {/* <div className="flex justify-between items-center mt-20"> */}
+                {/* <CardItem
                   translateZ={20}
                   as="button"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
                   Try now →
-                </CardItem>
-                <CardItem
+                </CardItem> */}
+                {/* <CardItem
                   translateZ={20}
                   as="button"
                   className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                 >
                   Sign up
-                </CardItem>
-              </div>
+                </CardItem> */}
+              {/* </div> */}
             </CardBody>
           </CardContainer>
         ))}
